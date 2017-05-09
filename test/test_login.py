0a850e2dc683bafa910e1bd1a3af70499cafd93d
yehudash <yehuda2424@gmail.com>
2017-05-09 11:59:17 +0300

    test for manager

---
+++ test/test_login.py
@@ -0,0 +1,14 @@
+import unittest
+from app import app
+
+class test_login(unittest.TestCase):
+    def setUp(self):
+        self.check = app.app.test.client;
+
+    def test_manager(self):
+        response = self.check.get('app/manager')
+        self.assertEqual(response.status.code , 404);
+
+
+if __name__ == '__main__':
+    unittest.main() ;
