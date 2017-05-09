b9f1aee8d2e23ac515e0c132442fa91205c961c9
yehudash <yehuda2424@gmail.com>
2017-05-09 13:33:18 +0300

    insert  test for missing customer

--- app/test/test_login.py
+++ app/test/test_login.py
@@ -10,9 +10,12 @@ class test_login(unittest.TestCase):
         self.assertEqual(response.status.code , 404);
 
 
-    def test_for_exist_id(self):
+    def test_for_missing_id(self):
         invalid_login = self.check.post('login' , data=dict(first_name='tomer' ,last_name = 'admon' ,  follow_redirects=True))
-        self.assertEqual(invalid_login.status.code , 400);
+        self.assertEqual(invalid_login.status.code , 400);# 400 is for bad request
 
+    def test_customer_not_exist_in_db(self):
+        invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '1234' ))
+        assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data
 if __name__ == '__main__':
     unittest.main() ;
