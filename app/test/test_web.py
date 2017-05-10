a965de4423cd9a562ef84f4743b22b63792618d4
yehudash <yehuda2424@gmail.com>
2017-05-10 12:11:22 +0300

    insert  test for enter system

--- app/test/test_web.py
+++ app/test/test_web.py
@@ -10,9 +10,9 @@ class test_web(unittest.TestCase):
 
     def test_enter_system(self):
         self.browser.get('http://localhost/:5000')
-        browser.find_element_by_id("first_name").send_keys('tomer')
-        browser.find_element_by_id("last_name").send_keys('admon')
-        id = browser.find_element_by_id("id")\
+        self.browser.find_element_by_id("first_name").send_keys('tomer')
+        self.browser.find_element_by_id("last_name").send_keys('admon')
+        id = self.browser.find_element_by_id("id")\
         id.send_keys('1234')
         id.send_keys(Keys.ENTER)
         assert u'המצביע אינו מופיע בבסיס הנתונים' not in self.browser.page_source
