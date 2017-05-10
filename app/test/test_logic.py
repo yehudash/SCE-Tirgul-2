ef1d1995c5d9358c7b202298154b77036881d8c6
yehudash <yehuda2424@gmail.com>
2017-05-10 16:24:54 +0300

    check unit testing

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -16,9 +16,9 @@ class test_login(unittest.TestCase):
         invalid_login = self.check.post('login' , data=dict(first_name='tomer' ,last_name = 'admon' ,  follow_redirects=True))
         self.assertEqual(invalid_login.status_code , 400);# 400 is for bad request
 
-    # def test_customer_not_exist_in_db(self):
-    #     invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '1234' ))
-    #     assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data.decode('utf-8')
+    def test_customer_not_exist_in_db(self):
+        invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '1234' ))
+        assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data.decode('utf-8')
 
     # def test_skipped(self):
     #     self.fail("shouldn't happen")
