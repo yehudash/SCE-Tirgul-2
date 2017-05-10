24d7e535fb70483829dfb7ee10660d2d778acd92
yehudash <yehuda2424@gmail.com>
2017-05-10 12:14:52 +0300

    insert  wrong test for enter system

--- app/test/test_web.py
+++ app/test/test_web.py
@@ -12,10 +12,10 @@ class test_web(unittest.TestCase):
         self.browser.get('http://localhost/:5000')
         self.browser.find_element_by_id("first_name").send_keys('tomer')
         self.browser.find_element_by_id("last_name").send_keys('admon')
-        id = self.browser.find_element_by_id("id")\
+        id = self.browser.find_element_by_id("id")
         id.send_keys('1234')
         id.send_keys(Keys.ENTER)
-        assert u'המצביע אינו מופיע בבסיס הנתונים' not in self.browser.page_source
+        assert u'המצביע אינו מופיע בבסיס הנתונים' in self.browser.page_source
 
 
 
