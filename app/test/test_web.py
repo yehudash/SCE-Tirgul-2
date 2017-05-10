7a8fb567e00f51d47e1ba7990c0db673b9716d90
yehudash <yehuda2424@gmail.com>
2017-05-10 12:10:12 +0300

    insert  test for enter system

---
+++ app/test/test_web.py
@@ -0,0 +1,22 @@
+import unittest
+from app import app
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class test_web(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_enter_system(self):
+        self.browser.get('http://localhost/:5000')
+        browser.find_element_by_id("first_name").send_keys('tomer')
+        browser.find_element_by_id("last_name").send_keys('admon')
+        id = browser.find_element_by_id("id")\
+        id.send_keys('1234')
+        id.send_keys(Keys.ENTER)
+        assert u'המצביע אינו מופיע בבסיס הנתונים' not in self.browser.page_source
+
+
+
+
