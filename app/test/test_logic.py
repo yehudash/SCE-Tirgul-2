01313ba1e9dfbfc6fba4f165e81220020219f5e8
yehudash <yehuda2424@gmail.com>
2017-05-10 16:17:31 +0300

    check unit testing

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -7,24 +7,35 @@ from app import app
 class test_login(unittest.TestCase):
     def setUp(self):
         self.check = app.test_client()
+    #
+    # def test_manager(self):
+    #     response = self.check.get('app/manager')
+    #     self.assertEqual(response.status_code, 404);
+    #
+    # def test_for_missing_id(self):
+    #     invalid_login = self.check.post('login' , data=dict(first_name='tomer' ,last_name = 'admon' ,  follow_redirects=True))
+    #     self.assertEqual(invalid_login.status_code , 400);# 400 is for bad request
+    #
+    # def test_customer_not_exist_in_db(self):
+    #     invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '1234' ))
+    #     assert u'המצביע אינו מופיע בבסיס הנתונים' not in invalid_customer.data.decode('utf-8')
 
-    def test_manager(self):
-        response = self.check.get('app/manager')
-        self.assertEqual(response.status_code, 404);
-
-    def test_for_missing_id(self):
-        invalid_login = self.check.post('login' , data=dict(first_name='tomer' ,last_name = 'admon' ,  follow_redirects=True))
-        self.assertEqual(invalid_login.status_code , 400);# 400 is for bad request
-
-    def test_customer_not_exist_in_db(self):
-        invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '1234' ))
-        assert u'המצביע אינו מופיע בבסיס הנתונים' not in invalid_customer.data.decode('utf-8')
+    def test_skipped(self):
+        self.fail("shouldn't happen")
 
     def tearDown(self):
         del self.check
 
+
 if __name__ == '__main__':
     unittest.main() ;
+
+
+
+
+
+
+
 
 
 
