a93a36e5cedb6a16df575fad35a741428ecdaeb8
yehudash <yehuda2424@gmail.com>
2017-05-10 20:42:51 +0300

    check first 3 unit tests

--- app/test/test_web.py
+++ app/test/test_web.py
@@ -14,18 +14,18 @@ class test_web(unittest.TestCase):
 
         self.browser = webdriver.Firefox(executable_path="/home/linux/PycharmProjects/SCE-Tirgul-2/app/test/geckodriver")
 
-    def test_enter_system(self):
-        self.browser.get('http://localhost:5000')
-        first_name = self.browser.find_element_by_id("first_name")
-        last_name = self.browser.find_element_by_id("last_name")
-        id = self.browser.find_element_by_id("id")
-        first_name.send_keys('tomer')
-        last_name.send_keys('admon')
-        id.send_keys('123')
-        id.send_keys(Keys.RETURN)
-
-        # else :
-        assert u'המצביע אינו מופיע בבסיס הנתונים' not in self.browser.page_source or u'המשתמש הנל הצביע כבר' in self.browser.page_source
+    # def test_enter_system(self):
+    #     self.browser.get('http://localhost:5000')
+    #     first_name = self.browser.find_element_by_id("first_name")
+    #     last_name = self.browser.find_element_by_id("last_name")
+    #     id = self.browser.find_element_by_id("id")
+    #     first_name.send_keys('tomer')
+    #     last_name.send_keys('admon')
+    #     id.send_keys('123')
+    #     id.send_keys(Keys.RETURN)
+    #
+    #     # else :
+    #     assert u'המצביע אינו מופיע בבסיס הנתונים' not in self.browser.page_source or u'המשתמש הנל הצביע כבר' in self.browser.page_source
 
     def tearDown(self):
         self.browser.quit()
