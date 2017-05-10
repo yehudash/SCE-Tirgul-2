3810a7f65b7ecbf58875d48f49b535091d2bb0f4
yehudash <yehuda2424@gmail.com>
2017-05-10 20:50:39 +0300

    check first 3 unit tests

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -18,8 +18,7 @@ class test_login(unittest.TestCase):
 
     def test_customer_not_exist_in_db(self):
         invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '1234' ))
-        data_str = invalid_customer.data
-        assert u'המצביע אינו מופיע בבסיס הנתונים' in data_str
+        assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data.decode('utf-8')
 
     def tearDown(self):
         del self.check
