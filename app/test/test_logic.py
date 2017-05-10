55c8096256035be9c5a98ce0180f72cf9fb3bbd4
yehudash <yehuda2424@gmail.com>
2017-05-10 21:01:10 +0300

    check first 3 unit tests

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -17,8 +17,8 @@ class test_login(unittest.TestCase):
         self.assertEqual(invalid_login.status_code , 400);# 400 is for bad request
 
     def test_customer_not_exist_in_db(self):
-        invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id ='1234'  ,  follow_redirects=True))
-        assert 'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data
+        invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '0' ))
+        assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data.decode('utf-8')
 
     def tearDown(self):
         del self.check
