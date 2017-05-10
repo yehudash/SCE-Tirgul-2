ac9663f6c8cddfa43e52c3246da31bb72ccedb75
yehudash <yehuda2424@gmail.com>
2017-05-10 12:34:21 +0300

    insert  wrong test for enter system

--- app/test/_init_.py
+++ app/test/_init_.py
@@ -1,1 +1,8 @@
-
+#
+# import test
+#
+# class _init_(test):
+#     def test_exe(self):
+#         test.test_manager(self)
+#
+# test_exe(self)
--- app/test/test_web.py
+++ app/test/test_web.py
@@ -9,12 +9,14 @@ class test_web(unittest.TestCase):
         self.browser = webdriver.Firefox()
 
     def test_enter_system(self):
-        self.browser.get('http://localhost/:5000')
-        self.browser.find_element_by_id("first_name").send_keys('tomer')
-        self.browser.find_element_by_id("last_name").send_keys('admon')
-        id = self.browser.find_element_by_id("id")
-        id.send_keys('1234' + Keys.ENTER)
+        # self.browser.get('http://localhost/:5000')
+        # self.browser.find_element_by_id("first_name").send_keys('tomer')
+        # self.browser.find_element_by_id("last_name").send_keys('admon')
+        # id = self.browser.find_element_by_id("id")
+        # id.send_keys('1234' + Keys.ENTER)
         assert False
 
+if __name__ == '__main__':
+    unittest.main()
 
 
