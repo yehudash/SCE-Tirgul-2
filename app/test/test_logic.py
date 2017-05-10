d8dd7ee552034e2cb3c44199cdd8ab20364fecfe
yehudash <yehuda2424@gmail.com>
2017-05-10 16:28:23 +0300

    check unit testing

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -20,6 +20,7 @@ class test_login(unittest.TestCase):
         invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '1234' ))
         assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data.decode('utf-8')
 
+
     # def test_skipped(self):
     #     self.fail("shouldn't happen")
 
