5f2cd4008a9a5132d5745af0bbb37979bedf16d2
yehudash <yehuda2424@gmail.com>
2017-05-10 13:58:28 +0300

    check something

--- app/test/_init_.py
+++ app/test/_init_.py
@@ -1,10 +1,10 @@
-
-import test
-
-class _init_(test):
-    def test_exe(self):
-        test.test_login.test_manager()
-        test.test_web.test_enter_system()
-
-
-test.test_exe() ;
+#
+# import test
+#
+# class _init_(test):
+#     def test_exe(self):
+#         test.test_login.test_manager()
+#         test.test_web.test_enter_system()
+#
+#
+# test.test_exe() ;
--- app/test/test_web.py
+++ app/test/test_web.py
@@ -8,13 +8,13 @@ class test_web(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def test_enter_system(self):
-        # self.browser.get('http://localhost/:5000')
-        # self.browser.find_element_by_id("first_name").send_keys('tomer')
-        # self.browser.find_element_by_id("last_name").send_keys('admon')
-        # id = self.browser.find_element_by_id("id")
-        # id.send_keys('1234' + Keys.ENTER)
-        assert False
+    # def test_enter_system(self):
+    #     self.browser.get('http://localhost/:5000')
+    #     self.browser.find_element_by_id("first_name").send_keys('tomer')
+    #     self.browser.find_element_by_id("last_name").send_keys('admon')
+    #     id = self.browser.find_element_by_id("id")
+    #     id.send_keys('1234' + Keys.ENTER)
+    #     assert False
 
 if __name__ == '__main__':
     unittest.main()
