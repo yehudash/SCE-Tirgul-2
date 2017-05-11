dc01fb4fd992f42e8d38631cdee8245d4b11dc8b
yehudash <yehuda2424@gmail.com>
2017-05-11 21:00:53 +0300

    testing

--- app/test/test_web.py
+++ app/test/test_web.py
@@ -15,16 +15,16 @@ class test_web(unittest.TestCase):
         # nevigate to the application home page
         self.browser.get('http://localhost:5000/')
 
-    def test_enter_system(self):
-        first_name = self.browser.find_element_by_id("first_name")
-        last_name = self.browser.find_element_by_id("last_name")
-        id = self.browser.find_element_by_id("id")
-        first_name.send_keys('tomer')
-        last_name.send_keys('admon')
-        id.send_keys('123')
-        id.send_keys(Keys.RETURN)
-
-        assert u'המצביע אינו מופיע בבסיס הנתונים' not in self.browser.page_source or u'המשתמש הנל הצביע כבר' in self.browser.page_source
+    # def test_enter_system(self):
+    #     first_name = self.browser.find_element_by_id("first_name")
+    #     last_name = self.browser.find_element_by_id("last_name")
+    #     id = self.browser.find_element_by_id("id")
+    #     first_name.send_keys('tomer')
+    #     last_name.send_keys('admon')
+    #     id.send_keys('123')
+    #     id.send_keys(Keys.RETURN)
+    #
+    #     assert u'המצביע אינו מופיע בבסיס הנתונים' not in self.browser.page_source or u'המשתמש הנל הצביע כבר' in self.browser.page_source
 
     @classmethod
     def tearDown(self):
