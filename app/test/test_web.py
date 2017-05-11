d130cb4a9e067e99144ca41bf697cc3e45d3ebfa
yehudash <yehuda2424@gmail.com>
2017-05-11 20:55:25 +0300

    4 tests

--- app/test/test_web.py
+++ app/test/test_web.py
@@ -5,18 +5,15 @@ import os
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class test_web(unittest.TestCase):
     @classmethod
     def setUp(self):
         # create a new Firefox session
-        capabilities=DesiredCapabilities.FIREFOX
-        self.browser = webdriver.Firefox(executable_path="/usr/local/sbin/geckodriver")
-        s
+        self.browser = webdriver.Firefox()
         # nevigate to the application home page
-        self.browser.get('http://localhost:5000/' , capabilities)
+        self.browser.get('http://localhost:5000/')
 
     def test_enter_system(self):
         first_name = self.browser.find_element_by_id("first_name")
