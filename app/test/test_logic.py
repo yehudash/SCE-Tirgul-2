9d285c9f5637b13b29f92a864d5743564a15037f
Sali <salibenmocha@gmail.com>
2017-05-12 20:13:36 +0300

    test_invalid_user

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -7,15 +7,25 @@ from app import app
 class test_login(unittest.TestCase):
     @classmethod
     def setUp(self):
-        self.check = app.test_client()
+        self.check =app.test_client()
+
+    def cleanTest(selfself):
+        #clear the data after each test
+        app.data=()
+        app.free_id = 0
 
     def test_manager(self):
         response = self.check.get('app/manager')
         self.assertEqual(response.status_code, 404);
 
     def test_for_missing_id(self):
+        # this test ensures that you cannot get an access without id number
         invalid_login = self.check.post('login' , data=dict(first_name='tomer' ,last_name = 'admon' ,  follow_redirects=True))
         self.assertEqual(invalid_login.status_code , 400);# 400 is for bad request
+
+    def test_invalid_user(self):
+        invalid_user=self.check.post('/login', data = { 'first_name':'sali' , 'last_name': 'impostor', 'id':'2407'},follow_redirects=True)
+        return u'המצביע אינו מופיע בבסיס הנתונים' in invalid_user.data.decode("utf-8")
 
     # def test_customer_not_exist_in_db(self):
     #     invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '0' ))
