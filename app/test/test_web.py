dd98115744683188b98d21636a90db3a6715a74c
Sali <salibenmocha@gmail.com>
2017-05-13 15:08:27 +0300

    all 4 test testing

--- app/test/test_web.py
+++ app/test/test_web.py
@@ -14,7 +14,7 @@ class test_web(LiveServerTestCase):
     SQLALCHEMY_DATABASE_URI = "sqlite://"
     TESTING = True
     def create_app(self):
-        app = Flask(__name__)
+        # app = Flask(__name__)
         app.config['TESTING'] = True
         app.config['LIVESERVER_PORT'] = 8943
         app.config['LIVESERVER_TIMEOUT'] = 10
@@ -54,8 +54,9 @@ class test_web(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-        db.session.remove()
-        db.drop_all()
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
 
 
 
