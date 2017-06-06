fd6a5213dbfe8d766d52cdbfb475006f32a7f778
yehudash <yehuda2424@gmail.com>
2017-06-06 18:17:11 +0300

    test

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -38,14 +38,14 @@ class test_login(unittest.TestCase):
         # this test ensures that you cannot get an access without id number
         invalid_login = self.check.post('login' , data = { 'first_name':'tomer' , 'last_name': 'admon'} ,  follow_redirects=True)
         self.assertEqual(invalid_login.status_code , 400);# 400 is for bad request
-    #
-    # def test_invalid_user(self):
-    #     invalid_user=self.check.post('login', data = { 'first_name':'sali' , 'last_name': 'impostor', 'id':'2407' } ,  follow_redirects=True)
-    #     return u'המצביע אינו מופיע בבסיס הנתונים' in invalid_user.data
 
-    def test_customer_not_exist_in_db(self):
+    def test_invalid_user(self):
+        invalid_user=self.check.post('login', data = { 'first_name':'sali' , 'last_name': 'impostor', 'id':'2407' } ,  follow_redirects=True)
+        return u'המצביע אינו מופיע בבסיס הנתונים' in invalid_user.data.decode('utf-8')
+
+    # def test_customer_not_exist_in_db(self):
         invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '0' ))
-        assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data.decode('utf-8')
+    #     assert u'המצביע אינו מופיע בבסיס הנתונים' in invalid_customer.data.decode('utf-8')
 
 
     def tearDown(self):
