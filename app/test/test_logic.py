e0e9ea78d8f42bd370c34cd996db61d5602a51e9
yehudash <yehuda2424@gmail.com>
2017-06-06 18:31:02 +0300

    test

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -41,7 +41,8 @@ class test_login(unittest.TestCase):
 
     def test_invalid_user(self):
         invalid_user=self.check.post('login', data = { 'first_name':'sali' , 'last_name': 'impostor', 'id':'2407' } ,  follow_redirects=True)
-        return u'המצביע אינו מופיע בבסיס הנתונים' in invalid_user.data.decode('utf-8')
+        # return u'המצביע אינו מופיע בבסיס הנתונים' in invalid_user.data.decode('utf-8')
+        self.assertEqual(invalid_user.status_code , 404);
 
     # def test_customer_not_exist_in_db(self):
     # invalid_customer = self.check.post('login' , data = dict(first_name = 'impostor' , last_name='impostor' , id = '0' ))
