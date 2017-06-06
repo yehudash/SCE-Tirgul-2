d0e7253d0775d9ff813e507bd0066d291aad2e35
yehudash <yehuda2424@gmail.com>
2017-06-06 19:50:18 +0300

    from the begining

--- app/test/test_logic.py
+++ app/test/test_logic.py
@@ -7,11 +7,17 @@ from app import app,db
 from app.models import User, Party
 
 class test_login(unittest.TestCase):
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    #SQLALCHEMY_DATABASE_URI = "sqlite://"
     TESTING = True
     def create_app(self):
         app = Flask(__name__)
-        app.config['TESTING'] = True
+
+        class LoginTestCase(unittest.TestCase):
+            def setUp(self):
+                self.app = app
+        self.app.config['WTF_CSRF_ENABLED'] = False
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        #app.config['TESTING'] = True
         db.init_app(app)
         with app.app_context():
             db.create_all()
