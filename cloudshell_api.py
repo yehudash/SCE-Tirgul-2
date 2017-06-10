51a8b228c63804fd294e62a810eb5f5bbd3edf8f
salibenmocha <salibenmocha@gmail.com>
2017-06-10 16:02:37 +0300

    Create cloudshell_api.py

--- cloudshell_api.py
+++ cloudshell_api.py
@@ -15,8 +15,6 @@ resources = session.GetReservationDetails(reservation_id).ReservationDescription
 my_resource = [resource for resource in resources
                if resource.ResourceModelName == DEPLOYED_APP_MODEL]
 
-if len(my_resource) > 1:
-    raise Exception('There are more then one app in the sandbox')
 
 if len(my_resource) == 0:
     raise Exception('There are more then one app in the sandbox')
