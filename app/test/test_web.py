d8cacbfd7625c0d1f39fa92a5075007659f68044
Sali <salibenmocha@gmail.com>
2017-06-18 17:14:35 +0300

    test 1

--- app/test/test_web.py
+++ app/test/test_web.py
@@ -1,72 +1,93 @@
-
 # -*- coding: utf-8 -*-
-
-import os
+import sys
 import unittest
+import os
 from flask import Flask
+from app import app,db
 from app.models import User, Party
-from app import app , db
+from selenium.webdriver.common.keys import Keys
+from flask_config import basedir
+from selenium import webdriver
+from flask_testing import LiveServerTestCase
 
-
-class test_web(unittest.TestCase):
+class test_web(LiveServerTestCase):
     SQLALCHEMY_DATABASE_URI = "sqlite://"
     TESTING = True
-
-    def setUp(self):
-        #db.init_app(app)
-        self.check = app.test_client()
+    def create_app(self):
+        #app = Flask(__name__)
+        app.config['TESTING'] = True
+        app.config['LIVESERVER_PORT'] = 8943
+        app.config['LIVESERVER_TIMEOUT'] = 10
+        db.init_app(app)
         with app.app_context():
             db.drop_all()
             db.create_all()
             self.insert_data_to_db()
-        #return app
+        return app
 
     def insert_data_to_db(self):
         db.session.commit()
-        admon = User('tomer', 'admon', '123')
-        avoda = Party(u'העבודה',
-                      'https://www.am-1.org.il/wp-content/uploads/2015/03/%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94.-%D7%A6%D7%99%D7%9C%D7%95%D7%9D-%D7%99%D7%97%D7%A6.jpg')
-        db.session.add(avoda)
-        db.session.add(admon)
+        a = User('tomer', 'admon', '123')
+        v = Party(u'העבודה','https://www.am-1.org.il/wp-content/uploads/2015/03/%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94.-%D7%A6%D7%99%D7%9C%D7%95%D7%9D-%D7%99%D7%97%D7%A6.jpg')
+        db.session.add(v)
+        db.session.add(a)
         db.session.commit()
 
-    def test_manager(self):
-        response = self.check.get('app/manager')
-        self.assertEqual(response.status_code, 404)
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
+        # nevigate to the application home page
+        self.browser.get(self.get_server_url())
 
-    def test_for_missing_id(self):
-        # this test ensures that you cannot get an access without id number
-        invalid_login = self.check.post('login', data={'first_name': 'tomer', 'last_name': 'admon'},
-                                        follow_redirects=True)
-        self.assertEqual(invalid_login.status_code, 400);  # 400 is for bad request
+    def test_good_login(self):
+        first_name = self.browser.find_element_by_id("first_name").send_keys('tomer')
+        last_name = self.browser.find_element_by_id("last_name").send_keys('admon')
+        id = self.browser.find_element_by_id("id").send_keys('123' +Keys.RETURN )
+        assert u'המצביע אינו מופיע בבסיס הנתונים' not in self.browser.page_source or u'המשתמש הנל הצביע כבר' in self.browser.page_source
 
-    def test_invalid_user(self):
-        invalid_user = self.check.post('login', data={'first_name': 'sali', 'last_name': 'impostor', 'id': '2407'},
-                                       follow_redirects=True)
-        return u'המצביע אינו מופיע בבסיס הנתונים' in invalid_user.data.decode('utf-8')
+    def test_bad_login(self):
+        first_name = self.browser.find_element_by_id("first_name")
+        last_name = self.browser.find_element_by_id("last_name")
+        id = self.browser.find_element_by_id("id")
+        first_name.send_keys('bad')
+        last_name.send_keys('bad')
+        id.send_keys('bad')
+        id.send_keys(Keys.RETURN)
+        assert u'המצביע אינו מופיע בבסיס הנתונים' in self.browser.page_source
 
-########################new##################################
-    # def test_for_correct_vote(self):
-    #     valid_user = self.check.post('login', data={'first_name': 'tomer', 'last_name': 'admon', 'id': '123'},follow_redirects=True)
-    #     selected_party = self.check.post('index', data={'party_name': 'הליכוד'},follow_redirects=True)
-    #     party_from_db= Party.query.filter_by(p=selected_party.name).first()
-    #     old_vote = party_from_db.votes # the current votes
-    #     self.find_element_by_css_selector('submitbote').click() #now the user click select
-    #     new_vote = party_from_db.votes
-    #     #Now we will compare the number of old votes plus one and the new number of votes
-    #     self.assertEqual((new_vote),(old_vote +1))
+        ########################new##################################
+    def test_for_correct_vote(self):
+        first_name = self.browser.find_element_by_id("first_name").send_keys('tomer')
+        last_name = self.browser.find_element_by_id("last_name").send_keys('admon')
+        id = self.browser.find_element_by_id("id").send_keys('123' + Keys.RETURN)
+        selected_party=self.browser.find_element_by_id("img").send_keys(Keys.ENTER)
+        party_from_db = Party.query.filter_by(p=selected_party.name).first()
+        old_vote = party_from_db.voted  # the current votes
+        self.browser.find_element_by_id("btn").submit()
+        new_vote = party_from_db.voted
+        # Now we will compare the number of old votes plus one and the new number of votes
+        self.assertEqual((new_vote), (old_vote + 1))
+
+        #########################################################
 
 
-    #########################################################
     def tearDown(self):
-        del self.check
+        self.browser.quit()
         with app.app_context():
-            db.session.remove()
             db.drop_all()
-
+            db.session.remove()
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+
+
+
+
+
 
 
