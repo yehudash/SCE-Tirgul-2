689ae1ae3dabb58a8d52637bf08d9829544d7a23
yehudash <yehuda2424@gmail.com>
2017-06-22 10:41:18 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,3 @@
-sudo yum install git-all -y
-
 #install git python and devtools
 sudo yum -y install git-all centos-relese-SCL python-setuptools python-setuptools-devel python-devel
 sudo yum -y groupinstall "Development Tools"
@@ -10,3 +8,10 @@ sudo easy_install pip
 #clone proejct repo
 git clone https://github.com/yehudash/SCE-Tirgul-2.git
 cd SCE-Tirgul-2
+
+#install our app requirements
+sudo pip install -r requirements.txt requirements
+
+#create db
+python db_create.py
+
