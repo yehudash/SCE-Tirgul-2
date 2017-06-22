3b021b82e734b4a41247e93414b84249e3547607
yehudash <yehuda2424@gmail.com>
2017-06-22 13:22:06 +0300

    Update cloudshell_api.py

--- cloudshell_api.py
+++ cloudshell_api.py
@@ -1,22 +1,28 @@
 from cloudshell.api.cloudshell_api import CloudShellAPISession
 import sys
 
-server_ip = sys.argv[1] ##'172.31.22.239' ## This is the internal IP of our CloudShell in AWS
+server_ip = sys.argv[1]
 reservation_id = sys.argv[2]
-DEPLOYED_APP_MODEL = 'Generic Deployed App'
+DEPLOYED_APP_MODEL = 'Generic App Model'
 
 session = CloudShellAPISession(server_ip,
                                sys.argv[3],
                                sys.argv[4],
-                               sys.argv[5])  ##make sure 
-										  ##to pass these credentials from jenkins and don't store them in GitHub!!
+                               sys.argv[5])
 
 resources = session.GetReservationDetails(reservation_id).ReservationDescription.Resources
+
 my_resource = [resource for resource in resources
                if resource.ResourceModelName == DEPLOYED_APP_MODEL]
-
 
 if len(my_resource) > 1:
     raise Exception('There are more then one app in the sandbox')
 
-print my_resource[0].FullAddress 
+if len(my_resource) == 0:
+    raise Exception('There are no deployed application in the sandbox')
+
+resource_att = session.GetResourceDetails(my_resource[0].Name).ResourceAttributes
+for item in resource_att:
+    if item.Name == 'Public IP':
+	sys.stdout.write(item.Value)
+        break
