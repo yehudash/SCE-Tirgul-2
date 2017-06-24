1efe44d84259142a5e7f03d188f6c27062c1333a
yehudash <yehuda2424@gmail.com>
2017-06-25 00:00:19 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -20,8 +20,5 @@ sudo pip install -r requirements.txt
 #create db
 python db_create.py
 
-#redirects all traffic from port 80 to 5000
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
-
 #run app
 nohup python run.py > ../log.txt 2>&1 </dev/null &
